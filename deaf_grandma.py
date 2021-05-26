05c23b9a3591ffef6629c171a4f600a16698809b
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-05-26 17:22:13 -0400

    Rewrote deaf grandma from js version, use a .upper() comparison to input string to catch lowercase with a counter sub loop to catch the exit GOODBYE! condition

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,21 @@
+
 def deaf_grandma():
-
-
+  current_counter = 0
+  print("Your grandmather awaits...")
+  print("WELL, OUT WITH IT!")
+  while current_counter < 2:
+    user_input = input("Test your grandmother with your nonsense \n")
+    if user_input == "":
+      print("WHAT?!")
+    elif user_input.upper() != user_input:
+      print("SPEAK UP, KID!")
+    elif user_input == "GOODBYE!":
+      current_counter = current_counter +1    
+      if current_counter == 2:
+        print("LATER, SKATER!")
+        return
+      print("LEAVING SO SOON?")
+    else:
+      print("NO NOT SENSE 1946!")
 
 deaf_grandma()
